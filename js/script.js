7e1eab9b623b31b208ba25e74225496f427a1802
Arianna Carotti <arianna.carotti@gmail.com>
2021-03-18 17:19:37 +0100

    corrette variabili

--- js/script.js
+++ js/script.js
@@ -3,13 +3,13 @@
 var nomeUtente = prompt ('Ciao dimmi il tuo nome');
 console.log(nomeUtente)
 
-cognomeUtente = prompt ('Ora il tuo cognome');
+var cognomeUtente = prompt ('Ora il tuo cognome');
 console.log(cognomeUtente)
 
-colorePreferito = prompt ('Il tuo colore preferito è');
+var colorePreferito = prompt ('Il tuo colore preferito è');
 console.log(colorePreferito)
 
-annoCorrente = '21';
+var annoCorrente = '21';
 
 var password = nomeUtente + cognomeUtente + colorePreferito + annoCorrente;
 console.log(password)
